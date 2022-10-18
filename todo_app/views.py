3b4c03901752f3d1dda576cf851829222c1b9243
bieehoang <devbieehoang.io@gmail.com>
2022-10-18 18:35:16 +0700

    modified:   db.sqlite3 	new file:   todo_app/.views.py.swp 	modified:   todo_app/__pycache__/views.cpython-310.pyc 	modified:   todo_app/views.py

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import ToDoList, TodoItem
-
+from django.shortcuts import get_object_or_404
 
 class ListListView(ListView):
     model = ToDoList
@@ -13,9 +13,16 @@ class ItemListView(ListView):
     template_name = "todo_app/todo_list.html"
 
     def get_queryset(self):
-        return TodoItem.objects.filter(todo_list_id=self.kwargs["pk"])
+        """
+        Restrict data which need to return. NOT OF ALL DATA
+        """
+        self.todo_list = get_object_or_404(ToDoList, id = self.kwargs["pk"]) # Check if data about todo item return not exist with ToDoList db
+        return TodoItem.objects.filter(todo_list=self.kwargs["pk"])
 
     def get_context_data(self):
+        """
+        Adding extra context
+        """
         context = super().get_context_data()
         context["todo_list"] = ToDoList.objects.get(id=self.kwargs["pk"])
         return context
