febfc4f92efd69aff56fd6dc3b83db455f52f0b4
Qiese <hoangtrungbi2002@gmail.com>
2022-11-30 19:02:07 +0700

    Alrealdy Finished Project

--- bamboo_todo/urls.py
+++ bamboo_todo/urls.py
@@ -1,22 +1,12 @@
-"""bamboo_todo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
+import os
+import environ
+from pathlib import Path 
+BASE_DIR =  Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(env('ADMIN_SITE'), admin.site.urls),
     path('', include("todo_app.urls")),
 ]
